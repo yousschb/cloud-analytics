058e3677688969694a0c06776df86e9a9ead30e3
yousschb <149575962+yousschb@users.noreply.github.com>
2024-03-15 09:38:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 from google.cloud import bigquery
 
+# Spécifiez le chemin vers votre fichier de clé d'API Google Cloud
+key_path = "https://github.com/yousschb/cloud-analytics/blob/main/caa-assignement-1-417215-e1c1db571b4e.json"
 
 # Créez un client BigQuery en utilisant la clé d'API
 client = bigquery.Client(project="caa-assignement-1-417215", credentials=key)
