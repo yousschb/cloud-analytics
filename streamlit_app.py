bf5f7c5fcedd0582131d9e02ad2e8e66732e0550
yousschb <149575962+yousschb@users.noreply.github.com>
2024-03-15 09:41:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,15 +3,17 @@ from google.cloud import bigquery
 # Spécifiez le chemin vers votre fichier de clé d'API Google Cloud
 key_path = "/Users/mac/Desktop/caa-assignement-1-417215-e1c1db571b4e.json"
 
-# Créez un client BigQuery en utilisant la clé d'API
-client = bigquery.Client(project="caa-assignement-1-417215", credentials=key)
+# Créez un client BigQuery en utilisant le fichier de clé d'API
+client = bigquery.Client.from_service_account_json(key_path)
 
-# Exécutez une requête SQL
+# Exemple de requête SQL
 query = """
 SELECT title
 FROM `caa-assignement-1-417215.Movies.Infos`
 WHERE LOWER(title) LIKE LOWER('terminator%')
 """
+
+# Exécuter la requête
 query_job = client.query(query)
 
 # Récupérez les résultats de la requête
@@ -20,7 +22,3 @@ results = query_job.result()
 # Affichez les résultats
 for row in results:
     print(row["title"])
-
-
-
-
