a9b5ecd44db7252861e7522c5bb1a7780c7c6e83
yousschb <149575962+yousschb@users.noreply.github.com>
2024-03-17 00:21:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,30 @@
-import requests
 from google.cloud import bigquery
 import streamlit as st
 
 # Spécifiez le chemin vers votre fichier de clé d'API Google Cloud
 key_path = "caa-assignement-1-417215-e1c1db571b4e.json"
 
-# Clé API TMDb
-TMDB_API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMWNmMjQ2MDE5MDkyZTY0ZDI1YWU1ZTNmMjVhMzkzMyIsInN1YiI6IjY1ZjU5ZTRlMDZmOTg0MDE3Y2M3Yzg3MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zF_TJxsIBuU9yHRlEQWEYYF7ZZg9ZoibgSnndHDhabA"
-
-# Créez un client BigQuery en utilisant le fichier de clé d'API Google Cloud
+# Créez un client BigQuery en utilisant le fichier de clé d'API
 client = bigquery.Client.from_service_account_json(key_path)
 
-# Fonction pour obtenir les détails du film à partir du TMDb
-def get_movie_details(tmdb_id):
-    url = f"https://api.themoviedb.org/3/movie/{tmdb_id}?language=en-US"
-    headers = {
-        "accept": "application/json",
-        "Authorization": f"Bearer {TMDB_API_KEY}"
-    }
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
+# Titre de l'application
+st.title("Movie Database Search")
 
-# Construction de la requête SQL de base avec les filtres des curseurs
-def build_query(search_query, selected_genre, average_rating, release_year):
+# Zone de recherche de titre de film
+search_query = st.text_input("Search for movie titles", "")
+
+# Liste déroulante pour sélectionner le genre de film
+genre_choices = ["---", "Action", "Comedy", "Drama", "Horror", "Science Fiction"]
+selected_genre = st.selectbox("Select genre", genre_choices)
+
+# Curseur pour sélectionner la note moyenne
+average_rating = st.slider("Select minimum average rating", min_value=0.0, max_value=5.0, step=0.1, value=3.0)
+
+# Curseur pour sélectionner l'année de sortie minimale
+release_year = st.slider("Select minimum release year", min_value=1900, max_value=2022, value=1980)
+
+# Construction de la requête SQL de base
+def build_query():
     base_query = """
     SELECT m.title, AVG(r.rating) as avg_rating
     FROM `caa-assignement-1-417215.Movies.Infos` AS m
@@ -47,37 +46,50 @@ def build_query(search_query, selected_genre, average_rating, release_year):
     
     return base_query
 
-def main():
-    # Titre de l'application
-    st.title("Movie Database Search")
-    
-    # Zone de recherche de titre de film
-    search_query = st.text_input("Search for movie titles", "")
-    
-    # Liste déroulante pour sélectionner le genre de film
-    genre_choices = ["---", "Action", "Comedy", "Drama", "Horror", "Science Fiction"]
-    selected_genre = st.selectbox("Select genre", genre_choices)
-    
-    # Curseur pour sélectionner la note moyenne
-    average_rating = st.slider("Select minimum average rating", min_value=0.0, max_value=5.0, step=0.1, value=3.0)
-    
-    # Curseur pour sélectionner l'année de sortie minimale
-    release_year = st.slider("Select minimum release year", min_value=1900, max_value=2022, value=1980)
-    
-    query = build_query(search_query, selected_genre, average_rating, release_year)
+
+# Exécuter la requête de filtrage et afficher les résultats
+def update_results():
+    query = build_query()
     if query.strip() == "":
-        st.write("Please provide search criteria.")
+        return "Please provide search criteria."
     else:
         query_job = client.query(query)
         results = query_job.result()
         if results.total_rows == 0:
-            st.write("No movies found matching the criteria.")
+            return "No movies found matching the criteria."
         else:
-            st.write("### Results:")
-            for row in results:
-                movie_title = row[0]
-                avg_rating = row[1]
-                st.write(f"- {movie_title} - Average Rating: {avg_rating}")
+            return results
 
-if __name__ == "__main__":
-    main()
+# Importation de la bibliothèque d'icônes
+from streamlit.components.v1 import html
+
+# Fonction pour générer des étoiles en fonction de la note
+def generate_stars(avg_rating):
+    if avg_rating is None:  # Vérification si la note est nulle
+        return "No rating available"
+    
+    filled_stars = int(avg_rating)
+    half_star = avg_rating - filled_stars >= 0.5
+    empty_stars = 5 - filled_stars - (1 if half_star else 0)
+    
+    stars_html = ""
+    for _ in range(filled_stars):
+        stars_html += "★ "
+    if half_star:
+        stars_html += "☆ "
+    for _ in range(empty_stars):
+        stars_html += "☆ "
+    
+    return stars_html
+
+# Bouton pour mettre à jour les résultats
+if st.button("Search"):
+    results = update_results()
+    if isinstance(results, str):
+        st.write(results)
+    else:
+        st.write("### Results:")
+        for row in results:
+            movie_title = row[0]
+            avg_rating = row[1]
+            st.write(f"- {movie_title} - Average Rating: {generate_stars(avg_rating)}")
