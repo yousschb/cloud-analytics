a2f6ac83c6fc17530b552d7a9d95155d0e69cf0a
yousschb <149575962+yousschb@users.noreply.github.com>
2024-03-16 22:22:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,4 @@
 import requests
-from google.cloud import bigquery
-import streamlit as st
-
-# Spécifiez le chemin vers votre fichier de clé d'API Google Cloud
-key_path = "caa-assignement-1-417215-e1c1db571b4e.json"
-
-# Clé API TMDb
-TMDB_API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMWNmMjQ2MDE5MDkyZTY0ZDI1YWU1ZTNmMjVhMzkzMyIsInN1YiI6IjY1ZjU5ZTRlMDZmOTg0MDE3Y2M3Yzg3MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zF_TJxsIBuU9yHRlEQWEYYF7ZZg9ZoibgSnndHDhabA"
-
-# Créez un client BigQuery en utilisant le fichier de clé d'API Google Cloud
-client = bigquery.Client.from_service_account_json(key_path)
-
-import requests
 import streamlit as st
 
 # Clé API TMDb
@@ -20,14 +7,17 @@ TMDB_API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMWNmMjQ2MDE5MDkyZTY0ZDI1YWU1ZTN
 def get_movie_details(tmdb_id):
     url = f"https://api.themoviedb.org/3/movie/{tmdb_id}?language=en-US"
     headers = {
-    "accept": "application/json",
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMWNmMjQ2MDE5MDkyZTY0ZDI1YWU1ZTNmMjVhMzkzMyIsInN1YiI6IjY1ZjU5ZTRlMDZmOTg0MDE3Y2M3Yzg3MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zF_TJxsIBuU9yHRlEQWEYYF7ZZg9ZoibgSnndHDhabA"
-}
+        "accept": "application/json",
+        "Authorization": f"Bearer {TMDB_API_KEY}"
+    }
     response = requests.get(url, headers=headers)
-    print(response.text)
-
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
 
 def main():
+    st.title("Movie Details")
     tmdb_id = st.text_input("Enter the tmdbId of the movie:")
     if st.button("Get Movie Details") and tmdb_id:  # Ajoutez un bouton pour déclencher la recherche
         movie_details = get_movie_details(tmdb_id)
@@ -36,3 +26,6 @@ def main():
             st.write(movie_details)
         else:
             st.write("No movie details found for the provided tmdbId.")
+
+if __name__ == "__main__":
+    main()
