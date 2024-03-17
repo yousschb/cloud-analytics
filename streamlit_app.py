806126fbdb97cad622737ba6c5660fedad3f64ea
yousschb <149575962+yousschb@users.noreply.github.com>
2024-03-17 02:23:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -103,8 +103,9 @@ def build_query(movie_name, selected_genre, average_rating, release_year):
     filters = []
     if movie_name:
         filters.append(f"LOWER(m.title) LIKE LOWER('%{movie_name}%')")
+    # Ajouter le filtre de genre uniquement si un genre est sélectionné
     if selected_genre != "---":
-        filters.append(f"LOWER(m.genres) LIKE LOWER('%{selected_genre}%')")
+        filters.append(f"','{selected_genre}','" in m.genres)
     filters.append(f"m.release_year >= {release_year}")
     
     if filters:
