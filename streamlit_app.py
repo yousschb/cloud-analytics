826a0dce128b12243fab2548756eb067fc492dc6
yousschb <149575962+yousschb@users.noreply.github.com>
2024-03-17 21:33:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ def main():
     selected_genre = st.selectbox("Select genre", genre_choices)
 
     # Liste des 10 langues les plus présentes dans la database
-    language_choices = ["All", "English", "French", "Japanese", "Italian", "Deutsh", "Espanol", "Swedish", "Finnish", "Chinese", "Russian"]
+    language_choices = ["All", "en", "fr", "ja", "it", "de", "es", "sv", "fi", "zh", "ru"]
     selected_language = st.selectbox("Select language", language_choices, key="language_select")
 
     # Curseur pour sélectionner la note moyenne
@@ -135,20 +135,9 @@ def build_query(movie_name, selected_genre, selected_language, average_rating, r
         else:
             # Si un seul mot-clé est fourni, ne pas ajouter de filtre supplémentaire
             filters.append(keyword_conditions[0])
-        
-   
-   # Définir les codes de langue correspondant dans le même ordre que les langues
-    language_choices = ["All", "English", "French", "Japanese", "Italian", "Deutsh", "Espanol", "Swedish", "Finnish", "Chinese", "Russian"]         
-    language_codes = ["---", "en", "fr", "ja", "it", "de", "es", "sv", "fi", "zh", "ru"]
 
-    # Récupérer l'indice de la langue sélectionnée dans language_choices
-    selected_language_index = language_choices.index(selected_language)
-
-    # Utiliser cet indice pour obtenir le code de langue correspondant dans language_codes
-    selected_language_code = language_codes[selected_language_index]
-
-    if selected_language_code != "---":
-        filters.append(f"m.language = '{selected_language_code}'")
+    if selected_language != "All":
+        filters.append(f"m.language = '{selected_language}'")
        
     if selected_genre != "All":
     # Si le genre sélectionné contient une barre verticale, on considère chacun des genres séparément
